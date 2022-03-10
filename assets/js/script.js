f702b86146e952c5a23283a6e676d67732950a10
J Semidey <mtsemidey@gmail.com>
2022-03-10 17:42:43 -0500

    array for questions

---
+++ assets/js/script.js
@@ -0,0 +1,44 @@
+// question array, questions as objects
+var questions = [
+    {
+        title: "Which is the correct way to reference an external stylesheet?",
+        choices: ["<style src='style.css'>",
+        "<stylesheet>styles.css</stylesheet>",
+        "<link rel='stylesheet' href='styles.css'>",
+        "<link rel='styles' href='styles.css'>"],
+        answer: "<link rel='stylesheet' href='styles.css'>"
+    },
+
+    {
+        title: "Which of the following is NOT a CSS display property type?",
+        choices: ["inline", "block", "inline-flex", "flex"],
+        answer: "inline-flex"
+    },
+
+    {
+        title: "Which of the following is NOT a primitve value in JavaScript?",
+        choices: ["Boolean", "Number", "Text", "BigInt"],
+        answer: "Text"
+    },
+
+    {
+        title: "Which comparison operator means 'Not Equal Value or Equal Type'?",
+        choices: ["==", "!=", "<=", "!=="],
+        answer: "!=="
+    },
+
+    {
+        title: "Which is NOT a valid loop in JavaScript?",
+        choices: ["for/while", "while", "for", "do/while"],
+        answer: "for/while"
+    },
+
+    {
+        title: "Where is the correct placement for a link to an external JavaScript sheet?",
+        choices: ["In the <head> section",
+        "After the <body> section",
+        "At the end of the <body> section",
+        "None of the above"],
+        answer: "At the end of the <body> section"
+    },
+]
