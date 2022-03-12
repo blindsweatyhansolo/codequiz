d83129f6b484f7326a7b40dc3485ce694ac61a0c
J Semidey <mtsemidey@gmail.com>
2022-03-11 21:53:01 -0500

    score view on highscore page added

--- assets/js/highscore.js
+++ assets/js/highscore.js
@@ -1,5 +1,20 @@
+var highScore = document.querySelector("#highScore")
 var clear = document.querySelector("#clear")
 var goBack = document.querySelector("#goBack");
+
+// get scores from localStorage, convert from string
+var allScores = localStorage.getItem("allScores");
+allScores = JSON.parse(allScores);
+
+// if allScores is not empty, create li element with object key
+if (allScores !== null) {
+    for (var i = 0;  i < allScores.length; i++) {
+        var listItem = document.createElement("li");
+        listItem.setAttribute("class", "scoreCard");
+        listItem.textContent = allScores[i].initials + " : " + allScores[i].score;
+        highScore.appendChild(listItem);
+    }
+}
 
 
 // click event to clear localStorage and reload page
